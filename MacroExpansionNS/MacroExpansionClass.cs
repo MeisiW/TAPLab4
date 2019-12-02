ca93070ccf1b8265fc68c68766df407d7fa494ba
Meisi <meisi.wafiq@hotmail.com>
2019-12-02 17:11:39 +0100

    creata classe MacroExpansion con iteratore

---
+++ MacroExpansionNS/MacroExpansionClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroExpansionNS
+{
+    public class MacroExpansionClass
+    {
+        public static IEnumerable<T> MacroExpansion<T>(IEnumerable<T> sequence, IEnumerable<T> newValues, T value)
+        {
+            if (sequence == null)
+                throw new ArgumentNullException(nameof(sequence));
+            if (newValues == null)
+                throw new ArgumentNullException(nameof(newValues));
+            return UnsafeMacroExpansion(sequence, newValues, value);
+        }
+
+        private static IEnumerable<T> UnsafeMacroExpansion<T>(IEnumerable<T> sequence, IEnumerable<T> newValues, T value)
+        {
+            foreach(var e in sequence)
+            
+                if (Equals(e, value))
+                {
+                    foreach (var v in newValues.ToArray())
+                        yield return v;
+                }
+                else
+                    yield return e;
+            
+        }
+    }
+}
